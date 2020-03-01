c4d4a556a866c9de52fc4f06a1b5b334e388a86a
Jonathan Keane <jkeane@gmail.com>
2020-03-01 17:11:25 -0600

    More docs, less custom code

--- R/tracking.R
+++ R/tracking.R
@@ -1,3 +1,10 @@
+#' Connect to the sqlite tracking DB
+#'
+#' @param db_path path to the db
+#' @param clean logical, should the db be unlinked and re-initialized
+#'
+#' @return connection
+#' @export
 connect <- function(db_path, clean = FALSE) {
   if (clean) {
     unlink(db_path)
@@ -17,23 +24,33 @@ schema <- data.frame(
   tweet_text = character(0),
   date_added = character(0),
   date_tweeted = character(0),
-  tweeted = logical(0),
+  tweeted = integer(0),
   # these will be null for now, but might be useful
   store_url = character(0),
   direct_url = character(0),
   flickr_url = character(0)
 )
 
-add_row_statement <- function(df, con) {
-  glue_sql(
-    "INSERT INTO tweets (", glue_collapse(glue("{colnames(df)}"), sep = ', '), ")",
-    "VALUES(", glue_collapse(glue("{{{colnames(df)}}}"), sep = ', '), ");",
-    .con = con,
-    .envir = df
-  )
+#' Queue photos for tweeting
+#'
+#' Queue photo(s) for tweeting. This inserts them into the db but does not
+#' actually tweet them out (yet)
+#'
+#' @param photo path to the photo
+#' @param con the databse to use
+#' @param orig_dir directory to place the original files
+#' @param proc_dir directory to place the processed files
+#'
+#' @return `TRUE` silently, if successful
+#' @export
+queue <- function(photo, con, orig_dir = "orig", proc_dir = "processed") {
+  df <- process_many(photo, orig_dir = orig_dir, proc_dir = proc_dir)
+
+  # add to DB
+  return(DBI::dbWriteTable(con, "tweets", df, append = TRUE))
 }
 
-queue <- function(photo, con, orig_dir = "orig", proc_dir = "processed") {
+process_one <- function(photo, orig_dir, proc_dir) {
   # extract exif
   exif_data <- exifr::read_exif(photo)
 
@@ -45,14 +62,16 @@ queue <- function(photo, con, orig_dir = "orig", proc_dir = "processed") {
   sanitize_exif(proced, exif_data)
   file.remove(photo)
 
-  # add to DB
-  df <- data.frame(
+  return(data.frame(
     orig_file = orig,
     tweet_file = proced,
     tweet_text = format_exif(exif_data),
     date_added = Sys.time(),
-    tweeted = FALSE
-  )
-  statement <- add_row_statement(df, con)
-  return(DBI::dbExecute(con, statement))
+    tweeted = FALSE,
+    stringsAsFactors = FALSE
+  ))
+}
+
+process_many <- function(photo, orig_dir, proc_dir) {
+  return(do.call(rbind, lapply(photo, process_one, orig_dir = orig_dir, proc_dir = proc_dir)))
 }
--- tests/testthat/test-tracking.R
+++ tests/testthat/test-tracking.R
@@ -1,44 +1,38 @@
 con <- connect(":memory:", clean = TRUE)
 
-test_that("add_row_statement works", {
-  out <- add_row_statement(
-    data.frame(
-      orig_file = "new.jpg",
-      tweet_file = "copy/new.jpg",
-      tweet_text = "whoa",
-      date_added = "2020-01-01",
-      date_tweeted = NA,
-      tweeted = 0
-    ),
-    con
-  )
-
-  expect_s4_class(out, "SQL")
-  expect_identical(
-    as.character(out),
-    paste0(
-      "INSERT INTO tweets (orig_file, tweet_file, tweet_text, date_added, date_tweeted, tweeted)",
-      "VALUES('new.jpg', 'copy/new.jpg', 'whoa', '2020-01-01', NULL, 0);"
-    )
-  )
-})
-
-test_that("Queueing", {
+test_that("Queueing and processing", {
   # setup
   dir.create(test_path("orig"))
   dir.create(test_path("processed"))
   file.copy(test_path("IMG_4907.jpg"), test_path("copy.jpg"))
 
-  expect_identical(
+  expect_true(
     queue(test_path("copy.jpg"), con, orig_dir = test_path("orig"), proc_dir = test_path("processed")),
-    1L
   )
 
+  # queue 1
   data_out <- DBI::dbGetQuery(con, "SELECT * FROM tweets")
   expect_identical(data_out$orig_file, file.path(test_path("orig"), "copy.jpg"))
   expect_identical(data_out$tweet_file, file.path(test_path("processed"), "copy.jpg"))
   expect_true(grepl("Fuji from Hakone II", data_out$tweet_text))
   expect_identical(data_out$tweeted, 0L)
+
+
+  # queue 2
+  file.copy(test_path("IMG_4907.jpg"), test_path("copy-1.jpg"))
+  file.copy(test_path("IMG_4907.jpg"), test_path("copy-2.jpg"))
+
+  expect_true(
+    queue(
+      c(test_path("copy-1.jpg"), test_path("copy-2.jpg")),
+      con,
+      orig_dir = test_path("orig"),
+      proc_dir = test_path("processed")
+    )
+  )
+
+  data_out <- DBI::dbGetQuery(con, "SELECT * FROM tweets")
+  expect_identical(nrow(data_out), 3L)
 
   # teardown
   unlink(
