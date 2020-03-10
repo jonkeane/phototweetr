ae44deba8da064810fb332e5e031b76e28c7cb1f
Jonathan Keane <jkeane@gmail.com>
2020-03-09 23:20:41 -0500

    A few more shiny updates

--- inst/shiny/tweetpreview/server.R
+++ inst/shiny/tweetpreview/server.R
@@ -3,23 +3,27 @@ library(DT)
 library(phototweetr)
 library(RSQLite)
 
+process_photos_df <- function(photo_df) {
+  photo_df$tweet_text <- paste(substring(photo_df$tweet_text, 0, 25), "...")
+  photo_df$orig_file <- gsub("orig/", "", photo_df$orig_file)
+  photo_df[photo_df$tweeted == 1, ]$tweeted <- "\U002714"
+  photo_df[photo_df$tweeted == 0, ]$tweeted <- ""
+
+  return(photo_df[,c("orig_file", "tweet_text", "tweeted")])
+}
+
 shinyServer(function(input, output, session) {
   options(DT.options = list(pageLength = 15))
+  # TODO: make reactive?
   con <- connect(file.path(calling_dir, "phototweetr.sql"))
   photos_full <- dbGetQuery(con, "SELECT rowid, * FROM tweets;")
   dbDisconnect(con)
 
-  print(file.path(calling_dir, "phototweetr.sql"))
-  print(photos_full)
-
-  photos <- photos_full[,c("orig_file", "tweet_text")]
-  photos$tweet_text <- paste(substring(photos$tweet_text, 0, 25), "...")
-
   output$photo_df <- DT::renderDataTable(
-    photos,
-    selection = list(mode = "single", selected = c(1)),
+    process_photos_df(photos_full),
+    selection = list(mode = "single", selected = 1),
     rownames = FALSE
-    )
+  )
   output$photo_selected <- renderText(input$photo_df_rows_selected)
 
   # TODO: edit some?
@@ -36,29 +40,51 @@ shinyServer(function(input, output, session) {
       photo_loc <- ""
     }
 
-    list(
-      src = photo_loc,
-      width = 500,
-      contentType = 'image/jpg'
-    )
+    return(list(src = photo_loc, width = 500))
   }, deleteFile = FALSE)
 
   observeEvent(input$tweet_now, {
     tweet_immediately(input$photo_df_rows_selected, photos_full)
+  })
+
+  observeEvent(input$show_image, {
+    show_photo(input$photo_df_rows_selected, photos_full)
   })
 })
 
 tweet_immediately <- function(id, photo_df) {
   photo_to_tweet <- photo_df[id, ]
 
+  if (photo_to_tweet$tweeted == 1) {
+    showModal(modalDialog(
+      h2("\U00274C This photo was already tweeted \U00274C"),
+      easyClose = TRUE
+    ))
+    return(NULL)
+  }
+
   message("Authenticating with Twitter")
   token <- auth_rtweet(set_renv = FALSE)
 
   message(glue::glue("Tweeting out photo {photo_to_tweet$orig_file}"))
-  photo_to_tweet <- tweet_photo(photo_to_tweet, path = calling_dir, token = token)
+  # photo_to_tweet <- tweet_photo(photo_to_tweet, path = calling_dir, token = token)
+  photo_to_tweet$tweeted <- 1L
 
   message("Updating the database")
   con <- connect(file.path(calling_dir, "phototweetr.sql"))
   update_one(photo_to_tweet, con)
   dbDisconnect(con)
+
+  showModal(modalDialog(h2("\U002714 The photo was tweeted \U002714"), easyClose = TRUE))
+}
+
+show_photo <- function(id, photo_df) {
+  photo_file <- file.path(calling_dir, photo_df[id, "orig_file"])
+  showModal(div(modalDialog(renderImage({
+    list(
+      src = photo_file,
+      "width" = "auto",
+      "height" = "auto"
+    )
+  }, deleteFile = FALSE), easyClose = TRUE), id = "photo_modal"))
 }
--- inst/shiny/tweetpreview/ui.R
+++ inst/shiny/tweetpreview/ui.R
@@ -7,17 +7,23 @@ navbarPage(
     'Tweet preview',
     tags$head(tags$style(
       type="text/css",
-      "#photo {overflow: hidden}",
+      "#photo { position: relative; overflow: hidden; border-radius: 15px;",
+      "maring-top: 10px; margin-bottom: 10px;}",
+      "#photo img {position: absolute; left: 50%; top: 50%;",
+      "-webkit-transform: translate(-50%,-50%);",
+      "-ms-transform: translate(-50%,-50%);",
+      "transform: translate(-50%,-50%);}",
+      "#photo_modal img {max-width: 100%; max-height: 100%;}"
     )),
     fluidRow(
       column(
         6, hr(),
         htmlOutput('tweet_text'),
-        imageOutput('photo', height = "500px"),
-        fluidRow(
-          align="bottom",
-          actionButton("tweet_now", "tweet immediately", width = "100%")
-        )
+        imageOutput('photo', height = "285px", width = "500px"),
+        actionButton("tweet_now", "tweet immediately", width = "500px"),
+        br(),
+        br(),
+        actionButton("show_image", "show full image", width = "500px")
       ),
       column(
         6,
